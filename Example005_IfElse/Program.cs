ec850cad4381fdff1bdd290ce0fa7eb6adc59bad
Karolina <karolina.kirilina@mail.ru>
2022-08-06 19:47:57 +0300

    Example005_IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Example005_IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя пользователя");
+            string username = Console.ReadLine();
+
+        if (username.ToLower() == "маша")
+            {
+                Console.WriteLine("Ура, это же Маша"); 
+            }
+        else
+        {
+            Console.Write("Привет, ");
+            Console.Write(username);
+        }
+        }
+    }
+}
