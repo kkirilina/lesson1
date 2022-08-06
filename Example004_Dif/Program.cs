b0e7862d1a8f482100b7648e1984dd3906bfa74b
Karolina <karolina.kirilina@mail.ru>
2022-08-06 19:20:13 +0300

    Example004_Dif

---
+++ Example004_Dif/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Example004_Dif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
