91f5076ed338503ac1441b213f1b19a35efff4b3
Karolina <karolina.kirilina@mail.ru>
2022-08-06 18:24:36 +0300

    Exaple001_HelloConsole

---
+++ Exaple001_HelloConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exaple001_HelloConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2 Hello World!");
+        }
+    }
+}
